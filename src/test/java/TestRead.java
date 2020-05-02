553a4669f71b7a59896f6ce9567eff8146ab9605
Catalin <volocaru.catalin17@gmail.com>
2020-05-02 12:59:20 +0100

    Provide tests for different scenarious

--- src/test/java/TestRead.java
+++ src/test/java/TestRead.java
@@ -10,8 +10,8 @@ public class TestRead {
 
     @Test
     public void testReadMatrix() throws IOException {
-        int[][] body = {{1, 2}, {3, 4}, {5, 6}};
-        Matrix expected = new Matrix(3, 2, body);
+        int[][] body = {{1, 2, 1}, {3, 4, 1}, {5, 6, 1}};
+        Matrix expected = new Matrix(3, 3, body);
         File file = new File("src/test/resources/matrix.txt");
         Matrix matrix = Reader.readMatrix(file);
         Assert.assertEquals(expected, matrix);
